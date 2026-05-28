9425431fc51e104d60d996f2a5a5172f16ce97f8
gaby001345-stack <gaby001345@gmail.com>
2026-05-27 20:46:23 -0600

    Creación de CustomInput para definir botones primarios, secundarios y terciarios.

--- MiAppTemas/App.tsx
+++ MiAppTemas/App.tsx
@@ -2,11 +2,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from './src/context/ThemeContext';
 import TabsNavigator from './src/navigation/TabsNavigator';
 export default function App() {
-return (
-<ThemeProvider>
-<NavigationContainer>
-<TabsNavigator />
-</NavigationContainer>
-</ThemeProvider>
+  return (
+  <ThemeProvider>
+    <NavigationContainer>
+      <TabsNavigator />
+      </NavigationContainer>
+      </ThemeProvider>
 );
 }
